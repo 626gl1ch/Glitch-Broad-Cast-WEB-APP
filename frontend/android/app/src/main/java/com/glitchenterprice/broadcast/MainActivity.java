6811440e16ef84697793a16dbf047161b2a7fca6
626gl1ch <glitchuserr@gmail.com>
2026-07-14 20:04:59 +0100

    Deploy comprehensive Settings Studio to gh-pages branch

--- frontend/android/app/src/main/java/com/glitchenterprice/broadcast/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.glitchenterprice.broadcast;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/src/components/PaywallModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-/**
- * Legacy Paywall Modal component
- * Replaced with 100% Free Web App Access.
- */
-export default function PaywallModal() {
-  return null;
-}
